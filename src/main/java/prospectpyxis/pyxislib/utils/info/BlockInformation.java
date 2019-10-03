39a7c81f3448855bc7f1e5a860dfc456103965f6
ProspectPyxis <prospectpyxis@gmail.com>
2019-10-03 18:03:08 +0700

    1.3.1 - TileEntity handling in BlockInformation

--- src/main/java/prospectpyxis/pyxislib/utils/info/BlockInformation.java
+++ src/main/java/prospectpyxis/pyxislib/utils/info/BlockInformation.java
@@ -3,19 +3,25 @@ package prospectpyxis.pyxislib.utils.info;
 import net.minecraft.block.Block;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.registries.IForgeRegistry;
+import prospectpyxis.pyxislib.block.BlockWithTileEntity;
 
 public class BlockInformation {
 
     public Block block;
     public String name;
     public ItemInformation itemBlockInfo;
+    public boolean hasTileEntity;
     public boolean isEnabled = true;
 
     public BlockInformation(Block block, String name, String model_name, int model_meta, Item itemBlock) {
         this.block = block;
         this.name = name;
         this.itemBlockInfo = new ItemInformation(itemBlock, name, model_name, model_meta);
+
+        if (this.block instanceof BlockWithTileEntity) this.hasTileEntity = true;
     }
 
     public BlockInformation(Block block, String name, String model_name, int model_meta) {
@@ -30,11 +36,27 @@ public class BlockInformation {
         this(block, block.getRegistryName().getResourcePath(), block.getRegistryName().getResourcePath(), 0, new ItemBlock(block).setRegistryName(block.getRegistryName().getResourcePath()));
     }
 
+    public BlockInformation setItemBlock(ItemBlock ib) {
+        this.itemBlockInfo = new ItemInformation(ib);
+        return this;
+    }
+
     public BlockInformation setEnabled(boolean configOption) {
         // configOption should point to a, well, config option that would disable this block
         this.isEnabled = configOption;
         this.itemBlockInfo.setEnabled(configOption);
         return this;
+    }
+
+    public Class getTileEntity() {
+        if (!hasTileEntity) return null;
+        else return ((BlockWithTileEntity)block).getTileEntityClass();
+    }
+
+    @SuppressWarnings("deprecation")
+    public void registerTileEntity() {
+        if (!hasTileEntity || !isEnabled) return;
+        GameRegistry.registerTileEntity(this.getTileEntity(), this.name);
     }
 
     public void registerBlock(IForgeRegistry<Block> registry) {
